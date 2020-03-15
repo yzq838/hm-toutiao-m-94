d4aed19b32c097e6fbdf08d950bf4c1839de4c61
yzq838 <550279807@qq.com>
2020-03-15 22:20:44 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'// 引入vant组件
+import 'vant/lib/index.css'// 引入vant样式
+Vue.use(Vant)// 全局注册调用vant导出对象install方法注册组件（任意位置使用）
 Vue.config.productionTip = false
 
 new Vue({
