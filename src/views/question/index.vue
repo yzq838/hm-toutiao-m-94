f11f6640fa5a245c10d9ad8b5756aeed4fb1c1d8
yzq838 <550279807@qq.com>
2020-03-19 00:26:12 +0800

    其他功能组件-骨架屏-基本编写和一级路由导航栏

--- src/views/question/index.vue
+++ src/views/question/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>问答模块</div>
+  <div>
+    <div class="split"></div>
+      <van-skeleton title avatar row="3" />
+       <div class="split"></div>
+      <van-skeleton title avatar row="3" />
+       <div class="split"></div>
+      <van-skeleton title avatar row="3" />
+       <div class="split"></div>
+      <van-skeleton title avatar row="3" />
+       <div class="split"></div>
+      <van-skeleton title avatar row="3" />
+
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div> 用户页面</div>
+  <div>个人中心</div>
 </template>
 
 <script>
