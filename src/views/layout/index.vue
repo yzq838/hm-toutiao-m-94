880e4bdd1e3f34aa941e2c60bff75c6f02727906
yzq838 <550279807@qq.com>
2020-03-19 00:02:05 +0800

    完成Layout组件首页底部区域

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="container">
-    <!-- 导航组件 给搜索加样式跳转到搜索页面 -->
-    <van-nav-bar @click-right="$router.push('/search')" fixed title="黑马头条" right-text="搜索" ></van-nav-bar>
-<!-- 一级路由容器 -->
-<router-view></router-view>
+    <!-- 导航组件 给搜索加样式跳转到搜索页面 页面地址是user时隐藏导航-->
+
+    <van-nav-bar @click-right="$router.push('/search')" fixed title="黑马头条" right-text="搜索" v-if="$route.path !== '/user'" ></van-nav-bar>
+<!-- 一级路由容器 地址是user 更改样式（）-->
+<div class="my-wrapper" :class="{noTop: $route.path === '/user'}">
+  <router-view></router-view>
+</div>
+<van-tabbar route>
+  <!-- 点击导航标签跳转路由 -->
+  <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
+        <van-tabbar-item icon="chat-o" to='/question'>问答</van-tabbar-item>
+        <van-tabbar-item icon="tv-o" to="/video">视频</van-tabbar-item>
+        <van-tabbar-item icon="user-circle-o" to="/user">我的</van-tabbar-item>
+</van-tabbar>
+<!-- 标签栏组件 -->
   </div>
 </template>
 
@@ -18,6 +29,7 @@ export default {
   width: 100%;
   height: 100%;
   position: relative;
+  // 控制二级路由容器的高宽
   .my-wrapper{
     width: 100%;
     height: 100%;
