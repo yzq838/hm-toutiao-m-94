769eca5ad0eb5e931debfe276cd50203e42c4d5a
yzq838 <550279807@qq.com>
2020-03-20 00:07:28 +0800

    提交表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,90 @@
 <template>
-<div class='container'>
+  <div class="container">
     <!-- 导航 显示返回箭头-->
     <!-- click-left点击左侧事件 -->
     <!-- $router.go(-1) $router.back() -->
-    <van-nav-bar title='登录'  left-arrow @click-left="$router.back()"></van-nav-bar>
-  <!-- 登录布局 外层需要cell-group组件来包裹提供 边框-->
-<van-cell-group>
-  <van-field label="手机号" placeholder="请输入手机号"></van-field>
-  <van-field label="验证码" placeholder="请输入验证码" >
-    <van-button slot="button" size="small" type="primary">发送验证码</van-button>
-  </van-field>
-</van-cell-group>
-<div class="login-box">
-  <van-button round size="small" type="info" block>登录</van-button>
-</div>
-
+    <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 登录布局 外层需要cell-group组件来包裹提供 边框-->
+    <van-cell-group>
+      <van-field
+      @blur="checkMobile"
+        :error-message="errorMessage.mobile"
+        v-model.trim="loginForm.mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+      ></van-field>
+      <van-field
+       @blur="checkCode"
+        :error-message="errorMessage.code"
+        v-model.trim="loginForm.code"
+        label="验证码"
+        placeholder="请输入验证码"
+      >
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="login-box">
+      <van-button @click="login" round size="small" type="info" block>登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 表单数据
+      loginForm: {
+        mobile: '13911111111', // 手机号
+        code: '246810' // 验证码
+      },
+      errorMessage: {
+        mobile: '', // 手机号提示消息
+        code: ''// 验证码提示消息
+      }
+
+    }
+  },
+  methods: {
+    checkMobile () {
+      if (!this.loginForm.mobile) {
+        this.errorMessage.mobile = '手机号不能为空'
+        return false
+      }
+      // 正则校验手机号格式
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errorMessage.mobile = '手机号格式不正确'
+        return false
+      }
+      this.errorMessage.mobile = ''
+      return true
+    },
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errorMessage.code = '验证码不能为空'
+        return false
+      }
+      // 正则校验验证码
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errorMessage.code = '验证码必须为6位数字'
+        return false
+      }
+      this.errorMessage.code = ''
+      return true
+    },
+    // 登录校验
+    login () {
+      if (this.checkMobile() && this.checkCode()) {
+        // 通过校验调用接口 对比用户名和密码是否正确
+      }
+    }
+  }
 
 }
 </script>
 
 <style lang='less' scoped>
 .login-box {
-  padding: 20px ;
+  padding: 20px;
 }
 </style>
