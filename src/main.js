7b669fde299466e117c074ba03eb7de4d304a8c4
yzq838 <550279807@qq.com>
2020-03-19 23:07:23 +0800

    修改vant主题蓝色

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import '@/permisstion'// 权限拦截模式
 import store from './store'
 import Vant from 'vant'// 引入vant组件
-import 'vant/lib/index.css'// 引入vant样式
+import 'vant/lib/index.less'// 引入vant样式
 import 'amfe-flexible'
 import '@/styles/index.less'// 引入全局样式
 Vue.use(Vant)// 全局注册调用vant导出对象install方法注册组件（任意位置使用）
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,6 +4,17 @@
     <!-- click-left点击左侧事件 -->
     <!-- $router.go(-1) $router.back() -->
     <van-nav-bar title='登录'  left-arrow @click-left="$router.back()"></van-nav-bar>
+  <!-- 登录布局 外层需要cell-group组件来包裹提供 边框-->
+<van-cell-group>
+  <van-field label="手机号" placeholder="请输入手机号"></van-field>
+  <van-field label="验证码" placeholder="请输入验证码" >
+    <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+  </van-field>
+</van-cell-group>
+<div class="login-box">
+  <van-button round size="small" type="info" block>登录</van-button>
+</div>
+
   </div>
 </template>
 
@@ -13,6 +24,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.login-box {
+  padding: 20px ;
+}
 </style>
