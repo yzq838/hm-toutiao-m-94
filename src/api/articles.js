d014a922856fb826ca4b51f55416af84aafcefad
yzq838 <550279807@qq.com>
2020-03-21 23:15:15 +0800

    封装获取文章列表APi

---
+++ src/api/articles.js
@@ -0,0 +1,8 @@
+// 处理文章模块请求数据
+import request from '@/utils/request'
+export function getArticles (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params }
+  })
+}
