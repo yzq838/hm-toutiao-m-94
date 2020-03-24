1b1b711a98a2bcab6a4455436a541a580b876572
yzq838 <550279807@qq.com>
2020-03-24 21:57:10 +0800

    删除我的频道api

--- src/api/channels.js
+++ src/api/channels.js
@@ -12,13 +12,13 @@ export function getMyChannels () {
       resolve({ channels: JSON.parse(str) }) // 这里的结构 是和 请求体中的结构
     } else {
       // 没有需要线上拉去缓存数据
-      request({ url: '/user/channels' }).then(result => {
-        // 获取 请求的结果
-        localStorage.setItem(key, JSON.stringify(result.channels))
-        // 这里还需要 resolve 我们的数据
+      request({
+        url: '/user/channels'
+      }).then(result => {
+        // 获取请求结果
+        localStorage.getItem(key, JSON.stringify(result.channels))
         resolve(result) // 这里表示直接 成功执行了 获取用户频道数据
-        // 频道数据 先把这个数据放入缓存
-      }) // 得到线上的一个用户频道列表数据
+      })
     }
   })
   // return request({
@@ -29,5 +29,24 @@ export function getMyChannels () {
 export function getAllChannels () {
   return request({
     url: '/channels'
+  })
+}
+// 删除频道api、@params 作为删除频道的依据
+export function delChannels (id) {
+  return new Promise(function (resolve, reject) {
+    const key = store.state.user.token ? CACHE_CHANNEL_V : CACHE_CHANNEL_T
+    const channels = JSON.parse(localStorage.getItem(key))
+    // 第一种方法
+    // channels = channels.filter(item => item.id !== id)
+    // localStorage.setItem(key, JSON.stringify(channels)
+    // 第二种方法
+    const index = channels.findIndex(item => item.id === id)
+    if (index > -1) {
+      channels.splice(index, 1)
+      localStorage.setItem(key, JSON.stringify(channels))
+      resolve()
+    } else {
+      reject(new Error('没有找到对应的频道...'))
+    }
   })
 }
