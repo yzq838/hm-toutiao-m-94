ea69c05ba5baaf81c081cfc91431ea8b662df47c
yzq838 <550279807@qq.com>
2020-04-04 23:22:43 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,12 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // config是所有cli中的webpack配置
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'
 }
