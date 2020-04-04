9dbc8e6552fa249999d22d8871b7828bf62597a5
yzq838 <550279807@qq.com>
2020-04-04 15:59:24 +0800

    小智同学基础布局

--- src/api/user.js
+++ src/api/user.js
@@ -1,50 +1,80 @@
-// 专门处理用户相关请求
-import request from '@/utils/request'// 引入请求模块
-
-// 登录方法
-
+/******
+ * 专门处理用户相关的请求
+ *
+ * ****/
+import request from '@/utils/request' // 引入请求模块
+// request相当于  axios的一个实例 和axios 有同样的方法和属性
+/***
+ * 登录方法
+ * ***/
 export function login (data) {
   return request({
+    //   配置选项
     url: '/authorizations', // 请求地址
     method: 'post',
-    data// 接收传入的data
+    data // 接收传入的data
+    // axios 中body参数放置在data中
   })
+  // 返回一个promise对象
 }
-// 关注用户
+/****
+ * 关注用户
+ * ***/
 export function followUser (data) {
   return request({
-    url: '/user/followings', // 关注用户请求地址
+    url: '/user/followings', // 关注用户
     method: 'post',
-    data// 接收传入的data
+    data
+    // body 参数  body data  query params
   })
 }
-// 取消关注用户
+
+/****
+ * 取消关注用户
+ * ***/
 export function unfollowUser (autid) {
   return request({
-    url: `/user/followings/${autid}`, // 关注用户请求地址
+    url: `/user/followings/${autid}`,
     method: 'delete'
-
   })
 }
-// 获取y用户自己个人信息
+/***
+ * 获取用户自己的个人信息 粉丝 数 /. 文章数
+ * ***/
 
 export function getUserInfo () {
   return request({
-    url: '/user' // 关注用户个人信息地址
-
+    url: '/user'
   })
 }
-// 获取用户的个人资料
+
+/****
+ * 获取用户的个人资料
+ *
+ * ***/
 export function getUserProfile () {
   return request({
     url: '/user/profile'
   })
 }
-// 修改用户头像信息
+/****
+ * 修改用户头像
+ * ****/
 export function updatePhoto (data) {
   return request({
     url: '/user/photo',
     method: 'patch',
-    data// body参数
+    data // body参数
+  })
+}
+/******
+ *  保存用户信息
+ *
+ * **/
+export function saveUserInfo (data) {
+  return request({
+    url: '/user/profile',
+    method: 'patch',
+    data: { ...data, photo: null } // 直接将photo设置为空
   })
 }
--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,40 +1,44 @@
 <template>
-   <div class="container">
-    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
+    <div class="container">
+    <van-nav-bar left-arrow @click-left="$router.back()" @click-right="saveUser" title="编辑资料" right-text="保存" ></van-nav-bar>
     <van-cell-group>
       <van-cell is-link title="头像"  center>
         <van-image
-        @click="showPhoto=true"
           slot="default"
           width="1.5rem"
           height="1.5rem"
           fit="cover"
           round
+          @click="showPhoto=true"
           :src="user.photo"
         />
       </van-cell>
-      <van-cell @click="showName = true" is-link title="名称" :value="user.name" />
-      <van-cell @click="showGender = true" is-link title="性别" :value="user.gender === 0 ? '男' : '女'"/>
-      <van-cell @click="showDate" is-link title="生日" :value="user.birthday" />
+      <van-cell is-link title="名称" @click="showName= true" :value="user.name" />
+      <van-cell is-link title="性别" @click="showGender = true" :value="user.gender === 0 ? '男' : '女' "/>
+      <van-cell is-link title="生日" @click="showDate" :value="user.birthday" />
     </van-cell-group>
-    <!-- 弹层组件 (头像弹层)-->
-    <van-popup v-model="showPhoto" style="width:80%" >
+    <!-- 放置 头像弹层 -->
+     <!-- 弹层组件 -->
+    <van-popup v-model="showPhoto" style="width:80%">
       <!-- 内容 -->
       <!-- 1 本地相册选择图片 -->
       <!-- 2 拍照 -->
        <van-cell @click="openFileDialog" is-link title="本地相册选择图片"></van-cell>
        <van-cell is-link title="拍照"></van-cell>
     </van-popup>
-     <!-- 弹昵称 -->
+    <!-- 放置昵称的弹层 -->
+      <!-- 弹昵称 -->
     <van-popup v-model="showName" style="width:80%" :close-on-click-overlay="false">
        <!-- 编辑用户昵称  双向绑定用户的昵称-->
-       <van-field :error-message ="nameMsg" v-model.trim = "user.name" type='textarea'  rows="4"></van-field>
-       <van-button block type="info" size='normal' @click="btnName">确定</van-button>
+       <!-- 直接用trim修饰符 去掉 名称的 前后空格 -->
+       <van-field :error-message="nameMsg" v-model.trim="user.name" type='textarea'  rows="4"></van-field>
+       <!-- 放置一个按钮 用来确定关闭弹层 -->
+       <van-button block type="info" size='normal' @click="btnName" >确定</van-button>
     </van-popup>
-    <!-- 性别弹层 注册性别弹层点击事件 -->
+    <!-- 性别弹层  注册性别弹层的选择事件-->
     <van-action-sheet @select="selectItem" :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
-<!-- sheng生日弹层 -->
-<van-popup v-model="showBirthDay" position="bottom">
+    <!-- 生日弹层 -->
+    <van-popup v-model="showBirthDay" position="bottom">
       <!-- 选择出生日期  出生日期应该小于现在时间-->
       <!-- type表示 当前的日期类型 年月日 -->
       <van-datetime-picker
@@ -42,91 +46,106 @@
            type="date"
           :min-date="minDate"
           :max-date="maxDate"
-          @cancel="showBirthDay = false "
-           @confirm="confirmDate"
+          @confirm="confirmDate"
+          @cancel="showBirthDay=false"
          />
     </van-popup>
-    <input type="file" ref="myFile" @change="upload" style="display:none">
+    <!-- 放置一个input:file 标签 用来上传图片  不能让人看到 隐藏掉 -->
+    <!-- vue中 可以通过 ref获取对象 -->
+    <!-- 如果选择了文件 就会触发input change事件 -->
+    <input @change="upload" ref="myFile" type="file" style="display:none" name="" id="">
   </div>
 </template>
 
 <script>
 import dayjs from 'dayjs'
-import { getUserProfile, updatePhoto } from '@/api/user'
-
+import { getUserProfile, updatePhoto, saveUserInfo } from '@/api/user'
+import { mapMutations } from 'vuex'
 export default {
   data () {
     return {
-      showPhoto: false, // 显示头像弹层
+      showPhoto: false, // 显示头像弹层:
       showName: false, // 显示昵称弹层
       showGender: false, // 显示性别弹层
-      showBirthDay: false, // 控制时间弹层
-      actions: [{ name: '男' }, { name: '女' }], // 性别数据
-      minDate: new Date(1900, 1, 1), // 最小时间
-      maxDate: new Date(), // 生日最大时间当前时间
-      currentDate: new Date(), // 当前时间（弹出层弹出时显示的时间）
+      showBirthDay: false, // 控制生日弹层
+      minDate: new Date(1900, 1, 1), // 生日组件 选择的最小日期
+      maxDate: new Date(), // 最大时间 当前时间
+      currentDate: new Date(), // 当弹出层时 显示的时间 是今天
+      actions: [{ name: '男' }, { name: '女' }], // 数组 表示弹层中的选项
       nameMsg: '', // 错误信息
       user: {
         // 放置个人资料信息
         name: '', // 用户昵称
         gender: 1, // 性别默认值
-        birthday: '', // 生日默认值
+        birthday: '2002-01-01', // 生日默认值
         photo: '' // 用户头像
       }
     }
   },
   methods: {
-
+    ...mapMutations(['updatePhoto']), // 引入 头像的方法
+    // 保存用户信息
+    async saveUser () {
+      try {
+        await saveUserInfo(this.user) // 传入user对象
+        this.$gnotify({ type: 'success', message: '保存成功' })
+      } catch (error) {
+        this.$gnotify({ message: '保存失败' })
+      }
+    },
     // 打开选择文件的对话框 触发点击input:file的动作
     openFileDialog () {
-      this.$refs.myFile.click()// 触发input:file的click事件 触发事件就会弹出文件对话框
+      this.$refs.myFile.click() // 触发input:file的click事件 触发事件就会弹出文件对话框
     },
-    async getUserProfile () {
-      this.user = await getUserProfile()// 获取用户资料
+    // 封装方法
+    async  getUserProfile () {
+      this.user = await getUserProfile()
     },
     btnName () {
-      if (this.user.name.length < 1 || this.user.name.length > 7) {
-        // 此时格式不正确
-        this.nameMsg = '用户昵称的长度为1-7个字符'
-        return
-      }
-      // 清空错误信息
-      this.nameMsg = ''
       // 关闭弹层
+      if (this.user.name.length < 1 || this.user.name.length > 7) {
+        // 此时说明长度不对 不符合要求
+        this.nameMsg = '用户昵称的长度应该是1-7的长度要求'
+        return // 不能够再继续
+      }
+      this.nameMsg = '' // 直接将错误信息清空
       this.showName = false
     },
     selectItem (item, index) {
-      this.user.gender = index// 直接把index赋值给性别
-      // 关闭弹层
+      this.user.gender = index
+      // 手动关闭 弹层
       this.showGender = false
-    }, // 显示生日弹层
+      // index 0 男  1 女
+      // 通过item或者 index可以得到 点击的是男或者是女
+    },
+    // 显示生日弹层
     showDate () {
-      this.showBirthDay = true // 控制时间弹层
-      // 将当前的生日设置到选择日期的当前时间，将生日字符串绑定到日期组件上
+      this.showBirthDay = true // 显示生日弹层
+      // 将当前的生日 设置到 选择日期的当前时间  将生日字符串 转化成Date对象 绑定到 日期组件上
       this.currentDate = new Date(this.user.birthday)
     },
     // 确定生日
     confirmDate () {
-      // 当前选择的生日其实就是currentDate
+      //  当前选择的生日 其实就是 currenDate
       // 拿到选择的日期  设置给生日  => date  => 字符串
-      this.user.birthday = dayjs(this.currentDate).format('YYYY-MM-DD')// 将date类型转化成字符串
-      // 关闭弹层
+      this.user.birthday = dayjs(this.currentDate).format('YYYY-MM-DD') // 将date类型转化成字符串
       this.showBirthDay = false // 关闭弹层
     },
-    async upload (params) {
-      // 完成头像上传
+    // 修改头像
+    async  upload (params) {
+      //  当选择 完头像之后 就可以修改头像
       const data = new FormData()
-      data.append('photo', this.$refs.myFile.files[0])
-      const result = await updatePhoto(data)
-      this.user.photo = result.photo
-      this.showPhoto = false
-    },
-
-    created () {
-      this.getUserProfile() // 获取用户资料
+      data.append('photo', this.$refs.myFile.files[0]) // 第二个参数 是 选择的图片文件 选择图片文件
+      const result = await updatePhoto(data) // 上传头像
+      this.user.photo = result.photo // 把成功上传的头像地址设置给当前data中的数据
+      // 修改头像成功之后  也去将 修改成功的头像 设置给当前的vuex
+      this.updatePhoto({ photo: result.photo }) // 将最新的头像地址设置给 vuex数据
+      this.showPhoto = false // 关闭头像弹层
     }
+  },
+  created () {
+    this.getUserProfile() // 获取用户资料
   }
-
 }
 </script>
 
