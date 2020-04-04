72832f2da2d12b10dd0f192bc422d01b275dee90
yzq838 <550279807@qq.com>
2020-04-04 22:15:58 +0800

    有条件的一级路由缓存

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <!-- 一级路由容器 -->
-    <router-view/>
+    <keep-alive>
+      <router-view v-if="$route.meta.isAlive" />
+    </keep-alive>
+
+    <router-view v-if="!$route.meta.isAlive"></router-view>
   </div>
 </template>
 
 <style lang="less">
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,10 @@ const routes = [
     component: Layout, // 布局组件 一级路由
     children: [{
       path: '', // 如果path什么都不写 默认就是二级路由的默认组件
-      component: home
+      component: home,
+      meta: {
+        isAlive: true // 是否缓存组件实例
+      }
     }, {
       path: '/question',
       component: question // 问答组件
